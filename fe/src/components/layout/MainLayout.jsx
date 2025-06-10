c6b4be7f38f5db54be60d5560702ab8366dba720
TuanVux <tuanvukg2703@gmail.com>
2025-06-10 09:58:45 +0700

    interface management Lecturer

--- fe/src/components/layout/MainLayout.jsx
+++ fe/src/components/layout/MainLayout.jsx
@@ -1,7 +1,5 @@
-
 import React from 'react';
-import { Box, CssBaseline, useMediaQuery } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import { Box, CssBaseline } from '@mui/material';
 import './MainLayout.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -9,19 +7,16 @@ import MainContainer from './MainContainer';
 import Footer from './Footer';
 
 const MainLayout = ({ children }) => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down(700)); // Màn hình < 700px
-
-  return (
-    <MainContainer>
-      <CssBaseline />
-      <Header />
-      {!isSmallScreen && <Sidebar />} {/* Ẩn Sidebar khi màn hình < 700px */}
-      <Box sx={{ mt: '64px' }}>
-        {children} {/* Render children passed from DynamicRouter */}
-      </Box>
-    </MainContainer>
-  );
+    return (
+        <MainContainer>
+            <CssBaseline />
+            <Header />
+            <Sidebar />
+            <Box sx={{ mt: '64px', ml: '350px' }}>
+                {children} {/* Render children passed from DynamicRouter */}
+            </Box>
+        </MainContainer>
+    );
 };
 
 export default MainLayout;
