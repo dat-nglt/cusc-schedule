fcdcab58a372da4425d8237cc14b834559993fef
Dhchg12 <Dhchg000@gmail.com>
2025-07-18 10:38:00 +0700

    test dăng nhập

--- be/src/services/authService.js
+++ be/src/services/authService.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { findUserByEmail, getUserId } from "./userService.js";
 import logger from "../utils/logger.js";
 
@@ -9,12 +12,25 @@ import logger from "../utils/logger.js";
  * @returns {string} - Mã JWT
  */
 export const generateAccessTokenService = (userId, role) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
   return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
-    expiresIn: "1h", // Token có hiệu lực trong 12 giờ
+    expiresIn: "1h", // Token có hiệu lực trong 1 giờ
   });
 };
 
+/**
+ * Tạo refresh token
+ * @param {string} userID - ID của người dùng
+ * @returns {string} - Refresh Token
+ */
 export const generateRefreshTokenService = (userID) => {
+  if (!process.env.JWT_REFRESH_SECRET) {
+    throw new Error("JWT_REFRESH_SECRET is not defined in environment variables");
+  }
+
   return jwt.sign({ id: userID }, process.env.JWT_REFRESH_SECRET, {
     expiresIn: "7d",
   });
@@ -23,12 +39,13 @@ export const generateRefreshTokenService = (userID) => {
 /**
  * Xác minh token JWT
  * @param {string} token - Chuỗi token cần xác thực
+ * @param {string} secret - Chuỗi bí mật dùng để xác thực
  * @returns {Object} - Dữ liệu giải mã từ token
  */
-export const verifyTokenService = (typeToken, secret) => {
+export const verifyTokenService = (token, secret) => {
   try {
-    const decode = jwt.verify(typeToken, secret);
-    return decode.id;
+    const decoded = jwt.verify(token, secret);
+    return decoded.id;
   } catch (error) {
     logger.error("Token verification failed:", error.message);
     throw new Error("accessToken không hợp lệ");
