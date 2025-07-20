24cbca307350b537e3a7e8fad93d79e42862ffba
TuanVux <tuanvukg2703@gmail.com>
2025-07-20 11:03:07 +0700

    tạo model bảng trung gian tạm thời chưa cần

--- be/src/models/ClassSubject.js
+++ be/src/models/ClassSubject.js
@@ -4,33 +4,37 @@ const ClassSubject = (sequelize) => {
   const ClassSubjectModel = sequelize.define(
     "ClassSubject",
     {
-      // Mã lớp học phần (khóa chính)
-      class_subject_id: {
+      class_id: {
         type: DataTypes.STRING(30),
         primaryKey: true,
         allowNull: false,
+        references: {
+          model: "classes",
+          key: "class_id",
+        },
       },
       // Tên lớp học phần
-      class_subject_name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      // Mã môn học liên kết
       subject_id: {
-        type: DataTypes.STRING(30),
+        type: DataTypes.STRING(50),
+        primaryKey: true,
         allowNull: false,
-      },
-      // Mã lớp học liên kết
-      class_id: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
+        references: {
+          model: "subjects",
+          key: "subject_id",
+        },
       },
     },
     {
-      tableName: "class_subjects", // Tên bảng trong CSDL
+      tableName: "class_sections", // Tên bảng trong CSDL
       timestamps: true, // Tự động thêm created_at và updated_at
       createdAt: "created_at", // Đặt tên cột createdAt
       updatedAt: "updated_at", // Đặt tên cột updatedAt
+      indexes: [
+        {
+          unique: true,
+          fields: ["class_id", "subject_id"], // Đảm bảo không có lớp học phần trùng lặp
+        },
+      ],
     }
   );
 
@@ -38,15 +42,19 @@ const ClassSubject = (sequelize) => {
   ClassSubjectModel.associate = (models) => {
     ClassSubjectModel.belongsTo(models.Subject, {
       foreignKey: "subject_id",
+      as: "subject", // Tên alias cho quan hệ
       onUpdate: "CASCADE",
       onDelete: "CASCADE", // Xóa tất cả lớp học phần liên quan nếu môn học bị xóa
     });
     ClassSubjectModel.belongsTo(models.Class, {
       foreignKey: "class_id",
+      as: "class", // Tên alias cho quan hệ
       onUpdate: "CASCADE",
       onDelete: "CASCADE", // Xóa tất cả lớp học phần liên quan nếu lớp học bị xóa
     });
   };
 
   return ClassSubjectModel;
-}
+};
+
+export default ClassSubject;
